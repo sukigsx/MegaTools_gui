cf3e35e29998143ddca073c5672ae4203c9aea33
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-02-11 06:36:10 +0100

    Update megatools.sh

--- megatools.sh
+++ megatools.sh
@@ -1,2 +1,117 @@
 #!/bin/bash
-zenity --info --text="hola tio"
+
+#toma el control al pulsar control + c
+trap ctrl_c INT
+function ctrl_c()
+{
+clear
+figlet -c Gracias por
+figlet -c utilizar mi
+figlet -c script
+wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
+exit
+}
+
+conexion(){
+if ping -c1 google.com &>/dev/null
+then
+    #echo ""
+    #echo -e " Conexion a internet [${verde}ok${borra_colores}]."
+    var_conexion="SI"
+    #echo ""
+else
+    #echo ""
+    #echo -e " Conexion a internet [${rojo}ko${borra_colores}]."
+    var_conexion="NO"
+    echo ""
+fi
+}
+
+software_necesario(){
+var_software="NO"
+echo -e " Verificando software necesario:"
+software="which git diff ping figlet yad" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
+for paquete in $software
+do
+which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa llamado programa
+sino=$? #recojemos el 0 o 1 del resultado de which
+contador="1" #ponemos la variable contador a 1
+    while [ $sino -gt 0 ] #entra en el bicle si variable programa es 0, no lo ha encontrado which
+    do
+        if [ $contador = "4" ] || [ $conexion = "no" ] 2>/dev/null 1>/dev/null 0>/dev/null #si el contador es 4 entre en then y sino en else
+        then #si entra en then es porque el contador es igual a 4 y no ha podido instalar o no hay conexion a internet
+            clear
+            echo ""
+            echo -e " ${amarillo}NO se ha podido instalar ${rojo}$paquete${amarillo}.${borra_colores}"
+            echo -e " ${amarillo}Intentelo usted con la orden: (${borra_colores}sudo apt install $paquete ${amarillo})${borra_colores}"
+            echo -e ""
+            echo -e " ${rojo}No se puede ejecutar el script sin el software necesario.${borra_colores}"
+            read pause
+            exit
+        else #intenta instalar
+            echo " Instalando $paquete. Intento $contador/3."
+            sudo apt install $paquete -y 2>/dev/null 1>/dev/null 0>/dev/null
+            let "contador=contador+1" #incrementa la variable contador en 1
+            which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa en tu sistema
+            sino=$? ##recojemos el 0 o 1 del resultado de which
+        fi
+    done
+echo -e " [${verde}ok${borra_colores}] $paquete."
+var_software="SI"
+done
+}
+
+actualizar_script(){
+archivo_local="megatools.sh" # Nombre del archivo local
+ruta_repositorio="https://github.com/sukigsx/MegaTools_gui.git" #ruta del repositorio para actualizar y clonar con git clone
+
+# Obtener la ruta del script
+descarga=$(dirname "$(readlink -f "$0")")
+#descarga="/home/$(whoami)/scripts"
+git clone $ruta_repositorio /tmp/comprobar >/dev/null 2>&1
+
+diff $descarga/$archivo_local /tmp/comprobar/$archivo_local >/dev/null 2>&1
+
+
+if [ $? = 0 ]
+then
+    #esta actualizado, solo lo comprueba
+    echo ""
+    #echo -e "${verde} El script${borra_colores} $0 ${verde}esta actualizado.${borra_colores}"
+    #echo ""
+    var_actualizado="SI"
+    chmod -R +w /tmp/comprobar
+    rm -R /tmp/comprobar
+else
+    #hay que actualizar, comprueba y actualiza
+    echo ""
+    echo -e "${amarillo} EL script${borra_colores} $0 ${amarillo}NO esta actualizado.${borra_colores}"
+    echo -e "${verde} Se procede a su actualizacion automatica.${borra_colores}"
+    sleep 3
+    mv /tmp/comprobar/$archivo_local $descarga
+    chmod -R +w /tmp/comprobar
+    rm -R /tmp/comprobar
+    echo ""
+    echo -e "${verde} El script se ha actualizado.${borra_colores}"
+    sleep 2
+    exit
+    #kill -9 $(ps -o ppid= -p $$)
+    #xdotool windowkill `xdotool getactivewindow`
+fi
+}
+
+conexion
+
+if [ $var_conexion = "SI" ]
+then
+    var_conexion="SI"
+    software_necesario
+    actualizar_script
+else
+    var_conexion="NO"
+    software_necesario
+    var_software="SI"
+    var_actualizado="Imposible comprobar sin conexion a internet"
+fi
+
+
