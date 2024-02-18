3aa2393880e6ac0dd6366da1f7b816e9f8982c80
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-02-18 09:47:19 +0100

    aa

--- megatools.sh
+++ megatools.sh
@@ -153,7 +153,10 @@ do
         "UTILIDADES PARA EL SISTEMA")
             zenity --error --title="MegaTools ( Diseñado por SUKIGSX )" --text="Selecciona una opcion de UTILIDADES PARA TU SISTEMA."
             ;;
-
+        "Instalacion de software")
+            #mete el pid del proceso a un archivo para poder matar este script desde otro
+            echo $$ > /tmp/ProcesoPidDeMegatools
+            bash InstalacionDeSoftware
         "Utilidades generales")
             zenity --info --title="Software-MegaTools ( Diseñado por SUKIGSX )" --text="Has seleccionado utilidades generales."
             ;;
--- popo.sh
+++ popo.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
+
+while :
+do
 # Mostrar un cuadro de diálogo con las opciones
-opciones=$(zenity --list --checklist --title="Selecciona las acciones a realizar" --column="Seleccionar" --column="Acción" \
-    FALSE "Actualizar paquetes (sudo apt update)" \
-    FALSE "Actualizar e instalar paquetes (sudo apt upgrade)" \
-    FALSE "Mostrar uso de memoria (free)" \
+opciones=$(zenity --list --checklist --title="Selecciona las acciones a realizar" --column="Seleccionar" --column="Acción" --column="Descripcion" \
+    FALSE "NAVEGADORES" "Si seleccionas esta, instalara todos los navegadores" \
+    FALSE "AUDIO - VIDEO" "" \
+    FALSE "ENTORNOS GRAFICOS LINUX" "" \
+    FALSE "UTILIDADES" "" \
+    FALSE "OFIMATICA" "" \
+    FALSE "SERVIDORES" "" \
+    FALSE "GESTOR DE PAQUETES" "" \
+    FALSE "PARA TU TERMINAL" "" \
+    FALSE "DISCOS" "" \
+    FALSE "JUEGOS" "" \
+    FALSE "SEGURIDAD" "" \
+    FALSE "MENSAJERIA" "" \
+    FALSE "Actualizar paquetes (sudo apt update)" "" \
+    FALSE "Actualizar e instalar paquetes (sudo apt upgrade)" "" \
+    FALSE "Mostrar uso de memoria (free)" "" \
+    --width=850 \
+    --height=650 \
+    --ok-label="Aceptar" \
+    --cancel-label="Atras" \
+    --extra-button="Ayuda instalacion software" \
+    --extra-button="Salir in confitmar" \
     --separator="|")
 
-# Verificar si se seleccionaron acciones
-if [ -z "$opciones" ]; then
-    zenity --error --text="No se seleccionó ninguna acción."
-    exit 1
+#Verificar si se seleccionaron acciones
+if [ $? -eq 0 ]; then
+    zenity --error --title="MegaTools ( Diseñado por SUKIGSX )" --text="No has seleccionado ninguna opcion del menu." --width=300
+else
+    zenity --question --title="MegaTools ( Diseñado por SUKIGSX )" --text="¿ Estás seguro de que deseas salir ?" --cancel-label="No" --ok-label="Si" --width=300
+    if [ $? -eq 0 ]; then
+        exit 0
+    fi
 fi
+
+
 
 # Recorrer las acciones seleccionadas y ejecutar los comandos correspondientes
 IFS="|"
@@ -24,7 +51,14 @@ for opcion in "${seleccionados[@]}"; do
             sudo apt upgrade;;
         "Mostrar uso de memoria (free)")
             free;;
+        "Salir in confitmar")
+            echo "uuuuuuuuuuuuuuuuuuuuuuuuuuu"; read p
+            ;;
+        "Ayuda instalacion software")
+            zenity --text-info --title="Ayuda - MegaTools" --filename=AyudaInstalacionSoftware --font="DejaVu Sans Mono" --width=650 --height=650
+            ;;
     esac
 done
 
-exit 0
+done
+exit
