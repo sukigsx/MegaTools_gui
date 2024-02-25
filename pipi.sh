3dedb0b62867f61a0c226b667cf53b4c3561f722
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-02-25 19:47:19 +0100

    Update pipi.sh

--- pipi.sh
+++ pipi.sh
@@ -205,7 +205,7 @@ if [ $var_conexion = "SI" ]; then
     fi
 fi
 
-echo "Ejecuto el restoaaaaaaaaaaaaaaaaaa"
+echo "Ejecuto el resto"
 echo "Actializado = $var_actualizado"
 echo "conexion a internet = $var_conexion"
 echo "software necesario = $var_software"
