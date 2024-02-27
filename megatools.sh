30ab4c414dc37c3f5c6f34d5c34259bd02d4775c
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-02-27 20:41:25 +0100

    Update megatools.sh

--- megatools.sh
+++ megatools.sh
@@ -170,8 +170,7 @@ fi
 while :
 do
     opcion=$(zenity --list --title="- MegaTools -" \
-    --text=" Version de MegaTools = $version\n Conexion a internet = $var_conexion\n software necesario para el correcto funcionamiento = $var_software\n Script esta actualizado = $var_actualizado\n\nInformacion de
-    SUKIGSX: \n Correo electronico = scripts@mbbsistemas.es\n Pagina web = https://repositorio.mbbsistemas.es\n" \
+    --text=" Version de MegaTools = $version\n Conexion a internet = $var_conexion\n software necesario para el correcto funcionamiento = $var_software\n Script esta actualizado = $var_actualizado\n\nInformacion de SUKIGSX: \n Correo electronico = scripts@mbbsistemas.es\n Pagina web = https://repositorio.mbbsistemas.es\n" \
     --column "Opciones del menu principal:" --column="Descripcion." \
     "UTILIDADES PARA EL SISTEMA" "" \
     "Crear/Borrar lanzador" "Te crea o brra el lanzador de tu escritorio." \
