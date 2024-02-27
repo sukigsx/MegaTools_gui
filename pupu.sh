35a333a807b4a37c3b44061eb9571ea340ff2552
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-02-27 17:40:09 +0100

    Update pupu.sh

--- pupu.sh
+++ pupu.sh
@@ -4,22 +4,24 @@ ruta_repositorio="https://github.com/sukigsx/MegaTools_gui.git" #ruta del reposi
 # Obtener la ruta del script
 descarga=$(dirname "$(readlink -f "$0")")
 #descarga="/home/$(whoami)/scripts"
-#git clone $ruta_repositorio /tmp/comprobar >/dev/null 2>&1
+git clone $ruta_repositorio /tmp/comprobar #>/dev/null 2>&1
 
-#diff $descarga/$archivo_local /tmp/comprobar/$archivo_local >/dev/null 2>&1
+diff $descarga/$archivo_local /tmp/comprobar/$archivo_local >/dev/null 2>&1
 
 
-#if [ $? = 0 ]
-#then
-#    var_actualizado="SI"
+if [ $? = 0 ]
+then
+    var_actualizado="SI"
+    echo "si esta actualizado"
 #    chmod -R +w /tmp/comprobar
 #    rm -R /tmp/comprobar
-#else
-#    var_actualizado="NO"
-#    git fetch origin
-#    git reset --hard origin/main
+else
+    var_actualizado="NO"
+    echo "no esta var_actualizado"
+    git fetch origin
+    git reset --hard origin/main
 #    chmod -R +w /tmp/comprobar
 #    rm -R /tmp/comprobar
-3fi
+fi
 
 echo "ruta de la variable descarga = $descarga"
