b823e7cdf69340ec638016b4afef750ec5cfb15e
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-03-02 09:50:48 +0100

    Update megatools.sh

--- megatools.sh
+++ megatools.sh
@@ -48,6 +48,7 @@ contador="1" #ponemos la variable contador a 1
             echo -e " ${amarillo}Intentelo usted con las ordenes: (${borra_colores}sudo apt update y sudo apt install $paquete ${amarillo})${borra_colores}"
             echo -e ""
             echo -e " ${rojo}No se puede ejecutar el script sin el software necesario.${borra_colores}"
+            salir="SI"
             exit
         else #intenta instalar
             echo " Instalando $paquete. Intento $contador/3."
