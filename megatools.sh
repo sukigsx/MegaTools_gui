7a8bd504fa026206e6cc0f5d08991655b83a2710
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-03-09 22:29:06 +0100

    a

--- megatools.sh
+++ megatools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="1.1.0"
+version="1.1.1"
 #software necesario para la ejecucion del programa
 software="which git diff ping figlet nano neofetch lsblk ethtool zenity wget gdebi find curl"
 
