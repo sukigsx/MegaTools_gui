f19f9893e91be4a891f705c6f6cded11a014a232
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-03-09 23:17:44 +0100

    Update megatools.sh

--- megatools.sh
+++ megatools.sh
@@ -298,6 +298,8 @@ do
             ;;
 
             "Instalacion de software")
+                ruta_ejecucion=$(dirname "$(readlink -f "$0")")
+                echo "$ruta_ejecucion"; read p
                 bash MenuPrincipal/InstalacionDeSoftware/InstalacionDeSoftware
                 ;;
 
