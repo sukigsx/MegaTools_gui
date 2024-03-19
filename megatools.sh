1289937faf1012a797eda3782dc1f04177a00d9a
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-03-19 11:23:40 +0100

    Update megatools.sh

--- megatools.sh
+++ megatools.sh
@@ -353,7 +353,7 @@ do
             ;;
 
         "Ayuda")
-            zenity --text-info --title="Ayuda - MegaTools -" --filename=$ruta_ejecucion/ayuda --font="DejaVu Sans Mono" --width=650 --height=650
+            zenity --text-info --title="Ayuda - MegaTools -" --filename=$ruta_ejecucion/Ayuda --font="DejaVu Sans Mono" --width=650 --height=650
             ;;
 
         *)
