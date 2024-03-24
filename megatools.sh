70ff7ec842ac90f4cbff3407c7c9ca3684596491
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-03-24 20:39:28 +0100

    a

--- megatools.sh
+++ megatools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ruta_ejecucion=$(dirname "$(readlink -f "$0")")
 
-version="1.00ss"
+version="1.00sddds"
 #software necesario para la ejecucion del programa
 software="which git diff ping figlet nano neofetch lsblk ethtool zenity wget gdebi find curl"
 
