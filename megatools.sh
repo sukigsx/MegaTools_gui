918282dcdcf87b715b88e92e3c6f75dbddd5047e
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-04-30 19:01:09 +0200

    a

--- megatools.sh
+++ megatools.sh
@@ -353,7 +353,7 @@ do
             ;;
 
         "Ayuda")
-            zenity --text-info --title="Ayuda - MegaTools -" --filename=$ruta_ejecucion/Megatools/AyudaPrincipal --font="DejaVu Sans Mono" --width=800 --height=600
+            zenity --text-info --title="Ayuda - MegaTools -" --filename=$ruta_ejecucion/Megatools/AyudaPrincipal --font="DejaVu Sans Mono" --width=1024 --height=600
             ;;
 
         *)
